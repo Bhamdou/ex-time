4bc5f439c58eb78ac63c7c2983ba03e062902827
Bahaa <qgunbb21@gmail.com>
2022-09-14 13:25:43 +0200

    first commit

---
+++ ex sleep.py
@@ -0,0 +1,56 @@
+# from time import sleep
+
+# sleep(1)
+# print("######   THIS   ######")
+# sleep(1)
+# print("######    IS    ######")
+# sleep(1)
+# print("######   SOME   ######")
+# sleep(1)
+# print("######   DOPE   ######")
+# sleep(1)
+# print("######  EXAMPLE ######")
+# sleep(1)
+# print("######   INNIT  ######")
+# sleep(1)
+# print("######   ????   ######")
+# sleep(1
+
+
+
+
+
+
+
+
+
+
+
+
+# from time import sleep
+# sleep(1)
+# print("progress #-5%")
+# sleep(1.5)
+# print("progress #-15%")
+# sleep(1)
+# print("progress #-25%")
+# sleep(1)
+# print("progress #-40%")
+# sleep(1)
+# print("progress #-55%")
+# sleep(1)
+# print("progress #-70%")
+# sleep(1)
+# print("progress #-85%")
+# sleep(1)
+# print("progress #-96%")
+# sleep(1)
+# print("progress #-100%")
+# sleep(1)
+# print("progress done ")
+
+
+# # for i in range(100):
+# #     sleep(0.5)
+# #     print("progress" + "" + str(i) + "%")
+
